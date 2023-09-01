87d7265890a93db364996af24bd16953567a26ca
Alonso58 <142807061+Alonso58@users.noreply.github.com>
2023-09-01 14:52:27 -0500

    Create Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 5; // Cambia este valor por el número que desees verificar
+
+        if (num > 0 && num % 2 != 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
