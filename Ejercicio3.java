ce3eac0d7fb92589b6e99d6ee433dc84ee1008df
Alonso58 <142807061+Alonso58@users.noreply.github.com>
2023-09-01 14:55:24 -0500

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int numA = 5; // Reemplaza 5 con el valor de numA que desees
+        int numB = 7; // Reemplaza 7 con el valor de numB que desees
+
+        if (numA > 0 && numB > 0) {
+            System.out.println("true");
+        }
+    }
+}
